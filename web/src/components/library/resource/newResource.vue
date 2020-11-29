adba6c2d8469c48f749751febaf46f0323631ecd
Alexhaoge <1810064@mail.nankai.edu.cn>
2020-11-29 12:00:24 +0800

    fix bug

--- web/src/components/library/resource/newResource.vue
+++ web/src/components/library/resource/newResource.vue
@@ -124,7 +124,7 @@ export default {
       })
     },
     getUrl () {
-      var url = 'http://localhost:8443/api/upload/' + this.$store.state.subject + '/' + this.$store.state.bookID + '/' + this.$store.state.sectionID + '/' + this.$store.state.lessonID
+      var url = process.env.BASE_URI + '/upload/' + this.$store.state.subject + '/' + this.$store.state.bookID + '/' + this.$store.state.sectionID + '/' + this.$store.state.lessonID
       return url
     },
     clickshow () {
